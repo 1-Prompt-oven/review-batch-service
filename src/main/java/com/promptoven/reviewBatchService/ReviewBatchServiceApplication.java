aca3ef4e114680596eca5f77d27b471fb8e193c9
김유석(Yu Seok Kim) <yuseok.kim@edushare.kr>
2024-11-26 10:59:37 +0900

    :bug: Add missing Annotation to connect Eureka

--- src/main/java/com/promptoven/reviewBatchService/ReviewBatchServiceApplication.java
+++ src/main/java/com/promptoven/reviewBatchService/ReviewBatchServiceApplication.java
@@ -3,8 +3,10 @@ package com.promptoven.reviewBatchService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableScheduling
 public class ReviewBatchServiceApplication {
 
