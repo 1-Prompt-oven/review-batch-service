f5b7ca6ea01623ca8f2870ceab640a617757bc83
ddu2 <flskhhdf@naver.com>
2024-11-28 17:44:10 +0900

    Feat : 판매자 전체 상품 평균 별점 집계 로직 추가

--- src/main/java/com/promptoven/reviewBatchService/dto/out/AggregateDto.java
+++ src/main/java/com/promptoven/reviewBatchService/dto/out/AggregateDto.java
@@ -11,16 +11,17 @@ import lombok.NoArgsConstructor;
 public class AggregateDto {
 
     private String productUuid;
+    private String sellerUuid;
     private Long reviewCount;
     private Double avgStar;
-
     private double previousTotalStar;
     private double newTotalStar;
 
     @Builder
-    public AggregateDto(String productUuid, Long reviewCount, Double avgStar, double previousTotalStar,
+    public AggregateDto(String productUuid, String sellerUuid, Long reviewCount, Double avgStar, double previousTotalStar,
             double newTotalStar) {
         this.productUuid = productUuid;
+        this.sellerUuid = sellerUuid;
         this.reviewCount = reviewCount;
         this.avgStar = avgStar;
         this.previousTotalStar = previousTotalStar;
@@ -30,6 +31,7 @@ public class AggregateDto {
     public AggregateEntity toEntity(AggregateDto aggregateDto) {
         return AggregateEntity.builder()
                 .productUuid(aggregateDto.getProductUuid())
+                .sellerUuid(aggregateDto.getSellerUuid())
                 .reviewCount(aggregateDto.getReviewCount())
                 .avgStar(aggregateDto.getAvgStar())
                 .build();
--- src/main/java/com/promptoven/reviewBatchService/infrastructure/AggregateRepository.java
+++ src/main/java/com/promptoven/reviewBatchService/infrastructure/AggregateRepository.java
@@ -1,13 +1,17 @@
 package com.promptoven.reviewBatchService.infrastructure;
 
 import com.promptoven.reviewBatchService.domain.AggregateEntity;
+import com.promptoven.reviewBatchService.dto.out.SellerAggregateDto;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AggregateRepository extends JpaRepository<AggregateEntity, Long> {
 
     List<AggregateEntity> findAllByProductUuidIn(List<String> productUuids);
 
     Optional<AggregateEntity> findByProductUuid(String productUuid);
+
+
 }
